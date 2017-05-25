eb035f60cc9779f4ed1870cff4720fdb2c1d4dbd
jamesfed <jamespreston@outlook.com>
2017-05-25 20:55:23 +0100

    Add files via upload

---
+++ Get-SympaLogin.ps1
@@ -0,0 +1,36 @@
+﻿function Get-SympaLogin
+{
+
+<#
+.Synopsis
+   This function gets a login cookie to use with the Sympa mail list server
+.DESCRIPTION
+   Use this function to login to the Sympa mail list server before calling other functions in this module (like Get-SympaMailListMembers).
+.EXAMPLE
+   $sympa = Get-SympaLogin -Username test-user@ox.ac.uk -Password cheesypassword1 -WSDL https://web.maillist.ox.ac.uk/ox/wsdl
+#>
+
+param(
+
+    [Parameter(Mandatory=$true,HelpMessage="Enter the username which has administrative permissions to your Sympa instance")]
+    [String]$Username,
+    [Parameter(Mandatory=$true,HelpMessage="Enter the password of the user previously entered")]
+    [String]$Password,
+    [Parameter(Mandatory=$true,HelpMessage="Enter the URI to the WSDL of the Sympa Server")]
+    [String]$WSDL
+
+    )
+
+    #Download the WDSL
+    $Sympa = New-WebServiceProxy -Uri $WSDL
+
+    #Make a cookie container
+    $Sympa.CookieContainer = New-Object System.Net.CookieContainer
+
+    #Login and get a session cookie
+    $Sympa.login($Username, $Password) | Out-Null
+    
+    #Output the result
+    return $Sympa
+
+}
---
+++ Get-SympaMailListMembers.ps1
@@ -0,0 +1,47 @@
+﻿function Get-SympaMailListMembers
+{
+
+<#
+.Synopsis
+   This function returns the members of a Mailing list(s)
+.EXAMPLE
+   Get-SympaMailListMembers -Sympa $Sympa -MailList queens-it
+.EXAMPLE
+   Get-SympaMailListMembers -Sympa $Sympa -MailList @('queens-it','queens-undergrads')
+#>
+
+param(
+
+    [Parameter(Mandatory=$true,HelpMessage="Pass in the result of the 'Get-SympaLogin' function")]
+    $Sympa,
+
+    [Parameter(Mandatory=$true,HelpMessage="Enter the name of the Mailing list(s) you want to return the members of")]
+    [Array]$MailingLists
+
+    )
+    
+    #Create empty collection
+    $Output = New-Object System.Collections.ArrayList
+
+    #Loop over the mail lists provided
+    foreach($MailList in $MailLists){
+        
+        $Results = $Sympa.review("$MailList")
+
+        #Parse the list into objects (Sympa only returns a big long string with line breaks)
+        foreach($Result in $Results){
+
+            #Build an object to store the results in
+            $item = New-Object -TypeName System.Object
+
+            #Add the members to the object
+            $item | Add-Member -MemberType NoteProperty -Name "Mailing list" -Value $MailList
+            $item | Add-Member -MemberType NoteProperty -Name "Member" -Value $Result
+
+            #Add the object to the collection
+            $Output.Add($item) | Out-Null
+        }
+    }
+
+    Return $Output
+}
