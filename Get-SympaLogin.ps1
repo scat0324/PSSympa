26f571c3297f82b078b494d0196f924281882f90
jamesfed <jamespreston@outlook.com>
2017-06-13 10:15:03 +0100

    Login now includes the option to use a creds file

--- Get-SympaLogin.ps1
+++ Get-SympaLogin.ps1
@@ -6,20 +6,52 @@
    This function gets a login cookie to use with the Sympa mailing list server
 .DESCRIPTION
    Use this function to login to the Sympa mailling list server before calling other functions in this module (like Get-SympaMailListMembers).
+
+   If using a credentials file (instead of storing them in the script) a sample of how the file should look is included with the module under the filename samplecredsfile.csv.
 .EXAMPLE
+   To login using the WSDL https://web.maillist.ox.ac.uk/ox/wsdl with the username test-user@ox.ac.uk and password cheesypassword1 use the following.
+
    $sympa = Get-SympaLogin -Username test-user@ox.ac.uk -Password cheesypassword1 -WSDL https://web.maillist.ox.ac.uk/ox/wsdl
+.EXAMPLE
+    To login using a CSV which stores the Username/Password/URI to WSDL use the following.
+
+    $sympa = Get-SympaLogin -CredsPath "C:\Sympa\credsfile.csv"
 #>
 
 param(
 
-    [Parameter(Mandatory=$true,HelpMessage="Enter the username which has administrative permissions to your Sympa instance")]
+    #This set of params are used when the details of the Username/Password/WSDL are in the script
+    [Parameter(Mandatory=$true,HelpMessage="Enter the username which has administrative permissions to your Sympa instance",ParameterSetName='CredsInScript')]
     [String]$Username,
-    [Parameter(Mandatory=$true,HelpMessage="Enter the password of the user previously entered")]
+    [Parameter(Mandatory=$true,HelpMessage="Enter the password of the user previously entered",ParameterSetName='CredsInScript')]
     [String]$Password,
-    [Parameter(Mandatory=$true,HelpMessage="Enter the URI to the WSDL of the Sympa Server")]
-    [String]$WSDL
+    [Parameter(Mandatory=$true,HelpMessage="Enter the URI to the WSDL of the Sympa Server",ParameterSetName='CredsInScript')]
+    [String]$WSDL,
+
+    #This set of params are used when the details of the Username/Password/WSDL are stored in a text file
+    [Parameter(Mandatory=$true,HelpMessage="Enter the path to the CSV containing credentials and the WSDL to use",ParameterSetName='CredsAtPath')]
+    [String]$CredsPath
 
     )
+
+    #If there is something at $CredsPath then test that the file is really there and if yes then asorb its contents into the script
+    if ($CredsPath -ne $Null)
+    {
+        #Check that the file is really there, stop the script if it isn't
+        try
+        {
+            $CredsFile = Import-Csv $CredsPath -ErrorAction Stop
+        }
+        catch
+        {
+            throw "Credentials file not found"
+        }
+
+        #Asorb the contents of the file into the script
+        $WSDL = $CredsFile.WSDL
+        $Username = $CredsFile.Username
+        $Password = $CredsFile.Password
+    }
 
     #Download the WDSL
     $Sympa = New-WebServiceProxy -Uri $WSDL
